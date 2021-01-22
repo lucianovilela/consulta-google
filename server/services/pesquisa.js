7af9f951edc255e6437833dbfbbb36df239b18dd
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2021-01-22 16:28:01 -0300

    nova versao

--- server/services/pesquisa.js
+++ server/services/pesquisa.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 const { google } = require("googleapis");
 const customsearch = google.customsearch("v1");
 const moment = require("moment");
-const  pretty  = require("pretty");
+const pretty = require("pretty");
 
 const db = require("../models/index");
 const SIGNOS = [
@@ -33,127 +33,101 @@ const getSigno = (dataNascimento) => {
   }
 };
 
-
-
-const getDateNascimentoBing = async (nome) => {
-  return await axios({url:`https://www.bing.com/search?q=${nome}&form=QBLH&sp=-1&pq=${nome}&sc=6-8&qs=n`, 
-    "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-      "cache-control": "no-cache",
-      "pragma": "no-cache",
-      "sec-fetch-dest": "document",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-site": "same-origin",
-      "sec-fetch-user": "?1",
-      "upgrade-insecure-requests": "1",
-    },
-    "referrerPolicy": "no-referrer-when-downgrade",
-    "body": null,
-    "method": "GET",
-    "mode": "cors"
-  }).then((res) => {
-    const html = res.data;
-    console.debug(pretty(html));
-
-    var dataNascimento=undefined;
-    var nomeGoogle = undefined;
-    txt = html.match(/Nasceu:<\/span>\s(\d{1,2}\sde\s[jfmajsond]\w+\sde\s\d{4})/gim);
-    if (txt) {
-      dataNascimento= moment(txt[0], "DD MMM YYYY", "pt-br");
-    }
-
-    txt = html.match(/<h2\sclass=\"\sb_entityTitle\">([\w\s]+)<\/h2>/gim);
-    if(txt){
-      nomeGoogle=txt[0];
-    }
-
-    return {dataNascimento, nomeGoogle};
-  });
-};
-
-
 const getDateNascimentoGoogle = async (nome) => {
   return await axios({
-    
-    url: `https://www.google.com/search?q=${nome}&rlz=1C1GCEU_pt-BRBR894BR894&ie=UTF-8`,
+    url: `https://www.google.com/search?q=${nome}&ie=UTF-8`,
     method: "GET",
   }).then((res) => {
     var html = res.data;
     let txt = html.match(/([jfajsondm]\w+\s\d+,\s\d+)[\s,]+/gim);
-    var dataNascimento=undefined;
+    var dataNascimento = undefined;
     var nomeGoogle = undefined;
 
     if (txt) {
-      dataNascimento= moment(txt[0], "MMMM DD YYYY", "en");
+      dataNascimento = moment(txt[0], "MMMM DD YYYY", "en");
     } else {
       txt = html.match(/(\d{1,2}\sde\s[jfmajsond]\w+\sde\s\d{4})/gim);
       if (txt) {
-        dataNascimento= moment(txt[0], "DD MMMM YYYY", "pt-br");
+        dataNascimento = moment(txt[0], "DD MMMM YYYY", "pt-br");
       }
     }
 
     txt = html.match(/data\-attrid\=\"title\".+<span>([\w\s]+)<\/span>/gim);
-    if(txt){
-      nomeGoogle=txt[0];
+    if (txt) {
+      nomeGoogle = txt[0];
     }
 
-    return {dataNascimento, nomeGoogle};
+    return { dataNascimento, nomeGoogle };
   });
 };
 
 const getPhotos = async (nome) => {
   try {
-    
-      const obj = await customsearch.cse.list({
-        q: nome,
-        cx: process.env.cx,
-        searchType: "image",
-        num: 1,
-        imgType: "photo",
-        fileType: "png",
-        safe: "off",
-        auth: process.env.developerKey,
-      });
+    const obj = await customsearch.cse.list({
+      q: nome,
+      cx: process.env.cx,
+      searchType: "image",
+      num: 1,
+      imgType: "photo",
+      fileType: "png",
+      safe: "off",
+      auth: process.env.developerKey,
+    });
 
-      return obj.data.items[0];
-    } catch (error) {
-      return {
-        link:"https://images.unsplash.com/photo-1519400197429-404ae1a1e184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1586&q=80",
-        error:error
-      }    
+    return obj.data.items[0];
+  } catch (error) {
+    return {
+      link:
+        "https://images.unsplash.com/photo-1519400197429-404ae1a1e184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1586&q=80",
+      error: error,
+    };
+  }
+};
+function retira_acentos(str) {
+  let com_acento =
+    "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
+
+  let sem_acento =
+    "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
+  let novastr = "";
+  for (i = 0; i < str.length; i++) {
+    let troca = false;
+    for (a = 0; a < com_acento.length; a++) {
+      if (str.substr(i, 1) == com_acento.substr(a, 1)) {
+        novastr += sem_acento.substr(a, 1);
+        troca = true;
+        break;
+      }
     }
-
-};
-
+    if (troca == false) {
+      novastr += str.substr(i, 1);
+    }
+  }
+  return novastr;
+}
 const sanitize = (nome) => {
-  return {
-    nome:nome.trim()
-    .toLowerCase()
-    .replace(/\s+/gim, " ")
-    .replace(/[^\w\s\dáéíóúâêîôûãõàèìòù]/gim, "")
-  };
-};
-const pesquisa = async (_nome) => {
-  const resultado = {
-    nome:_nome,
-    signo:undefined,
-    dataNascimento:undefined,
-    imagem:undefined,
-  };
+  return retira_acentos(
+      nome
+      .trim()
+      .toLowerCase()
+      .replace(/\s+/gim, " "));
+}
 
-  const nome = sanitize(_nome).nome;
+
+const pesquisa = async (_nome) => {
+
+  const nome = sanitize(_nome);
 
   let pesquisa = await db.consulta.findOne({ where: { nome: nome } });
   if (pesquisa) {
     return pesquisa;
   }
 
-  let {dataNascimento, nomeGoogle} = await getDateNascimentoGoogle(nome);
+  let { dataNascimento, nomeGoogle } = await getDateNascimentoGoogle(nome);
   try {
     var signo = undefined;
     var photo = undefined;
-    if(!nomeGoogle){
+    if (!nomeGoogle) {
       nomeGoogle = nome;
     }
     if (dataNascimento) {
